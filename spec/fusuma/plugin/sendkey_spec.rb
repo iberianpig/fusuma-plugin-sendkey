2cb8b5ca67c9a5d00e600e32f3c1e432fea13636
iberianpig <yhkyky@gmail.com>
2020-03-02 12:47:09 +0900

    Fix cops

--- spec/fusuma/plugin/sendkey_spec.rb
+++ spec/fusuma/plugin/sendkey_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe Fusuma::Plugin::Sendkey do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Fusuma::Plugin::Sendkey::VERSION).not_to be nil
   end
 end
